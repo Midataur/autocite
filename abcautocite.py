0023e93bd700403be76d9ca09181594ae4a80d68
Midataur <maxpetschack2@gmail.com>
2019-09-08 19:34:26 +1000

    Create abcautocite.py

---
+++ abcautocite.py
@@ -0,0 +1,58 @@
+#cites articles from abc news site in apa style
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = input('Input the url to cite: ')
+page = requests.get(url)
+soup = BeautifulSoup(str(page.content), 'html.parser')
+
+#div with some article info in it
+byline = soup.find(class_='byline')
+
+#find author
+#this regex matches the url of the author's page
+#example /news/simone-smale/12924081
+author = re.search(r'/news/[a-z]+-[a-z]+/\d+',str(byline))
+if author:
+    author = re.search(r'[a-z]+-[a-z]+',author.group(0))
+    author = author.group(0).split('-')
+
+#format nicely
+if author:
+    #do last name
+    nice_auth = author[1][0].upper()+author[1][1:]
+    #add first initial
+    nice_auth += ', '+author[0][0].upper()+'. '
+else:
+    nice_auth = 'N/A. '
+
+#find date
+time_line = soup.find(class_='timestamp')
+#this regex matches the abc date string format
+date = re.search(r'\w+ \d\d, \d\d\d\d',str(time_line))
+
+#format nicely
+if date:
+    date = date.group(0)
+    #get year
+    nice_date = '('+date[-4:]+', '
+    #do the month and day
+    m_d = re.match(r'\w+ \d\d',date).group(0).split()
+    month = m_d[0]
+    day = m_d[1][1] if m_d[1][0] == '0' else m_d[1]
+    nice_date += m_d[0]+' '+day+'). '
+else:
+    nice_date = '(N/A). '
+
+#find title
+title = soup.find('h1').contents[0]+'. '
+
+#format 'retrieved from'
+retr = 'Retrieved from '+url
+
+#put citation together
+citation = '**'+nice_auth+nice_date+title+'*ABC News*. '+retr+'**'
+
+#print the final citation
+print(citation)
